9c390c87d74758dc50f3dbec019cfeca5b2643d0
wllmvg <pipes.felipe425@gmail.com>
2025-04-01 20:19:30 -0500

    Estos son los operadores de JS

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-document.getElementById('demostracion').innerHTML = 'Este texto está en un archivo distinto'
+// operadores
+const variable = "Valor de la variable" // operador de asignacion
+
+const suma = 1 + 3 // + es operador aritmetico de la suma
+const resta = 1 - 3 // - es operador aritmetico de la resta
+const multiplicacion = 1 * 3 // * es operador de multiplicacion
+const division = 1 / 3 // / es operador de division
+const resto = 5 % 2 // % es operador que nos dara el residuo de una division
+const exponente = 3 ** 2 // ** es el operador de la exponenciacion
+
+let x = 5;
+let y = 5;
+
+x++; // suma 1 a x
+y--; // resta 1 a y
